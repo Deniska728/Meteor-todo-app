3d08d014e6ffeaa2dfc1ec8a53003c3715af531a
deniska728 <denis790504790@gmail.com>
2018-09-24 12:42:58 +0300

    test_commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import ReactDOM from 'react-dom';
+import { Tasks } from '../api/tasks';
+import Task from './Task';
+
+class App extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+ 
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+ 
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+ 
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+ 
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ))
+  }
+  render() {
+    console.log(this.props.tasks)
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <ul>
+          {this.renderTasks()}
+        </ul>
+        <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: 1 } }).fetch(),
+  };
+})(App);
